4f130c6e5ea17d19d754d23938f862281842c853
malo21st <44739330+malo21st@users.noreply.github.com>
2021-05-17 07:38:56 +0900

    Update report.py

--- report.py
+++ report.py
@@ -97,8 +97,8 @@ if df_data.size: # 0：検索結果がない場合，1以上：検索結果が�
         return f'[●]({url})'
 
     df_url = df_report['pdf']
-    df_url["html"]=df_url.apply(to_html, axis=1)
-    df_url["markdown"]=df_url.apply(to_markdown, axis=1)
+    df_url["html"] = df_report.apply(to_html, axis=1)
+    df_url["markdown"] = df_url.apply(to_markdown, axis=1)
     
     st.table(df_url)
     st.dataframe(df_url)
